9a53e662ae45a010882fb065fc63ee3d91963314
Manvi Jha <manvijha@Manvis-MacBook-Air.local>
2022-02-27 13:36:11 +0100

    create account working

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
+const bcrypt = require("bcrypt");
+
 
 //middleware
 app.use(cors());
@@ -12,12 +14,31 @@ app.post("/", async (req, res) => {
 
  const { name , email , pass , re_pass } =req.body;
 
+ const newUser1 = await pool.query("SELECT * FROM my_table WHERE user_email = $1", [email]);
+
+  if (newUser1.rows.length > 0) {
+    
+     res.status(401).json("User already exists !");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const bcryptPassword = await bcrypt.hash(pass, salt);
+  const bcryptRePassword = await bcrypt.hash(re_pass, salt);
+
+  // const validity = bcryptPassword.compare(bcryptRePassword);
+
+  if(pass != re_pass) {
+      res.status(403).json("Passwords do not match");
+  }
+
+  else{
+
      let newUser = await pool.query(
          "INSERT INTO my_table (user_name, user_email, user_password, confirm_password) VALUES ($1, $2, $3, $4) RETURNING *",
-         [ name, email, pass , re_pass ]
+         [ name, email, bcryptPassword , bcryptRePassword ]
      );
 
-     res.json(newUser.rows[0]);
+     res.json(newUser.rows[0]);}
  }
  catch (err) {
      console.error(err.message);
